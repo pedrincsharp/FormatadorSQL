6ce8aaf43746cceb2d2df60c15b5ceed1efd8f45
Pedrin <joao_pedro_alves_moraes@outlook.com.br>
2023-06-23 15:30:31 -0300

    Subind projeto completo

---
+++ FSQL/FormatSQL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using BasicSQLFormatter;
+
+namespace FormatadorSQL.FSQL
+{
+    public class FormatSQL
+    {
+        private string _path;
+        private SQLFormatter formatter;
+
+        public FormatSQL(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Arquivo informado não existe");
+
+            _path = path;
+        }
+
+        public string FormatFile()
+        {
+            try
+            {
+                string sql = GetSQLinFile();
+
+                if (string.IsNullOrWhiteSpace(_path))
+                    throw new Exception("Arquivo não contem código SQL válido!");
+
+                formatter = new SQLFormatter(sql);
+
+                return SaveSQLFile(formatter.Format());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
+        }
+
+        private string GetSQLinFile()
+        {
+            using(StreamReader sr = new StreamReader(_path))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        private string SaveSQLFile(string sql)
+        {
+            string fileFormatted = Path.GetDirectoryName(_path);
+            fileFormatted = Path.Combine(fileFormatted, Path.GetFileNameWithoutExtension(_path) + "_Formatado.SQL");
+            using(StreamWriter sw = new StreamWriter(fileFormatted))
+            {
+                sw.WriteLine(sql);
+            }
+            return sql;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using FormatadorSQL.FSQL;
+using System;
+using System.IO;
+
+namespace FormatadorSQL
+{
+    class Program
+    {
+        static FormatSQL formatSQL;
+
+        static void Main(string[] args)
+        {
+            string path = "";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Formatador de arquivo SQL");
+                Console.WriteLine("Digite o caminho ou arraste e solte o arquivo (.TXT|.SQL)");
+                path = Console.ReadLine();
+            }
+            else if (!string.IsNullOrWhiteSpace(args[0]))
+            {
+                path = args[0];
+            }
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                Console.WriteLine("Nenhum arquivo informado!");
+                return;
+            }
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Arquivo não existe!");
+                return;
+            }
+
+            switch (Path.GetExtension(path).ToUpper())
+            {
+                case ".TXT": case ".SQL":
+                    break;
+                default:
+                    Console.WriteLine("Extensão do arquivo é inválido!");
+                    return;
+            }
+
+            formatSQL = new FormatSQL(path);
+
+            formatSQL.FormatFile();
+
+        }
+    }
+}
